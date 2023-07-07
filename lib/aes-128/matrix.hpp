0baf400c8b82643686217a3e2d02841a2e09ba9f
uventura <ualivent@gmail.com>
2023-07-07 19:08:37 -0300

    Changing matrix data visibility

--- lib/aes-128/matrix.hpp
+++ lib/aes-128/matrix.hpp
@@ -5,15 +5,18 @@
 
 class Matrix {
 private:
-    std::vector<std::vector<unsigned int>> data;
     unsigned int rows;
     unsigned int cols;
 
 public:
+	std::vector<std::vector<unsigned int>> data;
+
     Matrix();
     Matrix(unsigned int numRows, unsigned int numCols);
+
     Matrix operator*(const Matrix& other) const;
     Matrix& operator=(const Matrix& other);
+
     void setElement(unsigned int row, unsigned int col, unsigned int value);
     void display() const;
 };
